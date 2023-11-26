2f17ea42b14851685ee367f1f23fe840bba385a2
coaxus-ux <trujillojulian.024@gmail.com>
2023-11-25 23:45:40 -0500

    proyect init

---
+++ src/main/java/ctictravel/ctictravel/CticTravelApplication.java
@@ -0,0 +1,13 @@
+package ctictravel.ctictravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CticTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CticTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ctictravel/ctictravel/CticTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package ctictravel.ctictravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CticTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
